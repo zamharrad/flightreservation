0b8d6ab938b0aef6556962ed70d30852c6faefbc
zamharrad <mhmdzamhar@gamil.com>
2020-08-06 11:41:22 +0530

    adding webSecurity configuraion class and Transactional

--- src/main/java/com/rad/flightreservation/security/config/WebSecurityConfig.java
+++ src/main/java/com/rad/flightreservation/security/config/WebSecurityConfig.java
@@ -2,23 +2,30 @@ package com.rad.flightreservation.security.config;
 
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configurable
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(HttpSecurity httpSecurity) {
         try {
-            httpSecurity.authorizeRequests().antMatchers("/showReg", "/", "index.html", "/registerUser", "/login", "/showLogin", "/login/*").permitAll()
-                    .antMatchers("/admin/showAddFlight").hasAnyAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable();
+            httpSecurity.authorizeRequests().antMatchers("/showReg", "/", "index.html", "/registerUser", "/login", "/showLogin","/findFlights", "/login/*")
+                    .permitAll().antMatchers("/admin/showAddFlight").hasAnyAuthority("admin")
+                    .anyRequest().authenticated().and().csrf().disable();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
     }
 }
--- src/main/java/com/rad/flightreservation/services/ReservationServiceImpl.java
+++ src/main/java/com/rad/flightreservation/services/ReservationServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -39,6 +40,7 @@ public class ReservationServiceImpl implements ReservationService {
     private String ITINERARY_DIR;
 
     @Override
+    @Transactional
     public Reservation bookFlight(ReservationRequest request) {
 
         LOGGER.info("Inside bookFlight()");
